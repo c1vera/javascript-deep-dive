30f45b8371bee6591704e8e076d5ae5e495c727d
Vera <yahwa2009@naver.com>
2024-01-03 16:43:21 +0900

    06장 정리

---
+++ 06-데이터 타입/ex06-10.js
@@ -0,0 +1,19 @@
+var template1 = '<ul>\n\t<li><a href="#">Home</a></li>\n</ul>';
+
+console.log(template1);
+/*
+<ul>
+  <li><a href="#">Home</a></li>
+</ul>
+*/
+
+var template2 = `<ul>
+  <li><a href="#">Home</a></li>
+</ul>`;
+
+console.log(template2);
+/*
+<ul>
+  <li><a href="#">Home</a></li>
+</ul>
+*/
